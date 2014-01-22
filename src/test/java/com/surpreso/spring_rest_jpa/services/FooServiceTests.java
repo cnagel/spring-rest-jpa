f67abe863c2a833eca5b5b29423815812369810b
Christoph Nagel <c.nagel@surpreso.com>
2014-01-22 15:27:51 +0100

    Extends FooService tests

--- src/test/java/com/surpreso/spring_rest_jpa/services/FooServiceTests.java
+++ src/test/java/com/surpreso/spring_rest_jpa/services/FooServiceTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.google.common.collect.Lists;
 import com.surpreso.spring_rest_jpa.DefaultConfig;
@@ -20,6 +21,7 @@ import com.surpreso.spring_rest_jpa.beans.Foo;
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { DefaultConfig.class }, loader = SpringApplicationContextLoader.class)
+@Transactional
 public class FooServiceTests {
 
 	@Inject
@@ -30,6 +32,54 @@ public class FooServiceTests {
 		ArrayList<Foo> foos = Lists.newArrayList(service.index());
 		assertEquals(2, foos.size());
 		assertEquals(new Long(1l), foos.get(0).getId());
+		assertEquals("Foo 1", foos.get(0).getName());
+	}
+
+	@Test
+	public void test_create() {
+		Foo foo = new Foo();
+		foo.setName("Foo 10");
+		Foo createdFoo = service.create(foo);
+		assertEquals(foo.getName(), createdFoo.getName());
+		assertNotNull(createdFoo.getId());
+	}
+
+	@Test
+	public void test_read() throws FooNotFoundException {
+		Foo foo = service.read(1l);
+		assertEquals(new Long(1l), foo.getId());
+		assertEquals("Foo 1", foo.getName());
+	}
+
+	@Test(expected = FooNotFoundException.class)
+	public void test_read_fooNotFound() throws FooNotFoundException {
+		service.read(3482394l);
+	}
+
+	@Test
+	public void test_update() throws FooNotFoundException {
+		Foo foo = new Foo();
+		foo.setName("Foo 9 new");
+		Foo updatedFoo = service.update(9l, foo);
+		assertEquals(new Long(9l), updatedFoo.getId());
+		assertEquals(foo.getName(), updatedFoo.getName());
+	}
+
+	@Test(expected = FooNotFoundException.class)
+	public void test_update_fooNotFound() throws FooNotFoundException {
+		Foo foo = new Foo();
+		foo.setName("Foo 9 new");
+		service.update(3482394l, foo);
+	}
+
+	@Test
+	public void test_delete() throws FooNotFoundException {
+		service.delete(1l);
+	}
+
+	@Test(expected = FooNotFoundException.class)
+	public void test_delete_fooNotFound() throws FooNotFoundException {
+		service.delete(473259l);
 	}
 
 }
